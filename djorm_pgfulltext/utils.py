39e5c39c92973f6919f9edf52974015729f44b83
Sebastian Hillig <bastih@users.noreply.github.com>
2016-02-18 23:55:39 +0100

    Update utils.py

--- djorm_pgfulltext/utils.py
+++ djorm_pgfulltext/utils.py
@@ -5,6 +5,8 @@ from django.utils.text import force_text
 
 
 def adapt(text):
+    # make sure connection is open
+    connection.cursor()
     a = psycopg2.extensions.adapt(force_text(text))
     a.prepare(connection.connection)
     return a
