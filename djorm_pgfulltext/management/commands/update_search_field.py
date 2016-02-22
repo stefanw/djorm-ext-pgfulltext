931fc031369882a4d8129d476361ba04077d99a5
Vojtěch Oram <vojtech@oram.cz>
2016-02-22 11:47:06 -0800

    manage update_search_field support for Django==1.9

--- djorm_pgfulltext/management/commands/update_search_field.py
+++ djorm_pgfulltext/management/commands/update_search_field.py
@@ -4,7 +4,7 @@ Update search fields.
 from __future__ import print_function
 from django.core.management.base import BaseCommand, CommandError
 from django.core.exceptions import ImproperlyConfigured
-from django.db import models
+from django.apps import apps
 
 
 class Command(BaseCommand):
@@ -18,7 +18,7 @@ class Command(BaseCommand):
         # check application
 
         try:
-            app_module = models.get_app(app)
+            app_module = apps.get_app_config(app)
         except ImproperlyConfigured:
             raise CommandError("There is no enabled application matching '%s'." % app)
 
@@ -27,13 +27,13 @@ class Command(BaseCommand):
         # get models
 
         if model:
-            m = models.get_model(app, model)
+            m = app_module.get_model(model)
             if not m:
                 raise CommandError("There is no model '%s'." % model)
 
-            app_models.append(models.get_model(app, model))
+            app_models.append(app_module.get_model(model))
         else:
-            app_models += models.get_models(app_module)
+            app_models += app_module.get_models()
 
         # get models only with search managers
 
